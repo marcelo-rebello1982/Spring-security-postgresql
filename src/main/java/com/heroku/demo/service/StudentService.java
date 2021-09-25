f9a88fd85bb4b077d26a485e21640fa354fcb35a
Marcelo Paulo <mp.rebello.martins@gmail.com>
2021-09-24 22:17:10 -0300

    refactor: change method loadUserByUsername

--- src/main/java/com/heroku/demo/service/StudentService.java
+++ src/main/java/com/heroku/demo/service/StudentService.java
@@ -1,22 +1,32 @@
 package com.heroku.demo.service;
 
 
+import com.heroku.demo.model.Student;
 import com.heroku.demo.repository.StudentRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class StudentService implements UserDetailsService {
+
+    private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-    private final StudentRepository studentRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return studentRepository.findByEmail(email);
+        Optional<Student> studentOptional = Optional.ofNullable(studentRepository.findByEmail(email));
+
+        if (studentOptional.isPresent()) {
+            return studentOptional.get();
+        }
+        throw new UsernameNotFoundException("Dados inválidos");
     }
+
 }
